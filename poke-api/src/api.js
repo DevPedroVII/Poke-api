25bb87157d295e19acb3687c9e26ced923d280f5
pedro Vinicius <singed161@gmail.com>
2023-03-06 22:18:55 -0300

    Create api.js

---
+++ poke-api/src/api.js
@@ -0,0 +1,8 @@
+export const searchPokemon = (pokemon) => {
+    try {
+        let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+        const response = fetch
+    } catch (error) {
+        console.log("error: ", error)
+    }
+}
