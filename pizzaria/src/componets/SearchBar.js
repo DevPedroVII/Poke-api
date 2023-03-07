4c138c9a2209e49c50c62e7e9837d5fc90178d48
pedro Vinicius <singed161@gmail.com>
2023-03-06 21:26:44 -0300

    Update SearchBar.js

--- pizzaria/src/componets/SearchBar.js
+++ pizzaria/src/componets/SearchBar.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 const SearchBar = () => {
+    const onChangeHandler = (e) => {
+        console.log("pokemon: ",e.target.value)
+    }
     return(
         <div className="searchbar-container">
             <div className="searchbar">
-              <input placeholder="Buscar Pokemon"/>
+              <input placeholder="Buscar Pokemon" onChange={onChangeHandler}/>
             </div>
         </div>
     )
