beb0cdf5e83ce6ba1f3a2ba930c1a36cb8e516de
pedro Vinicius <singed161@gmail.com>
2023-03-07 22:16:49 -0300

    Update App.js

--- poke-api/src/App.js
+++ poke-api/src/App.js
@@ -1,6 +1,8 @@
+import { searchPokemon } from './api';
 import './App.css';
 import NavBar from './componets/NavBar';
 import SearchBar from './componets/SearchBar';
+import (searchPokemon) from ".api"
 
 
 function App() {
