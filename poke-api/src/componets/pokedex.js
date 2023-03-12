0a4c44763111274c95d5af9de6a27968d89f216e
pedro Vinicius <singed161@gmail.com>
2023-03-11 22:27:30 -0300

    Update pokedex.js

--- poke-api/src/componets/pokedex.js
+++ poke-api/src/componets/pokedex.js
@@ -3,7 +3,9 @@ import React from "react";
 const Pokedex = () =>{
     return(
         <div>
+            <div>
             <h1>Pokedex</h1>
+            </div>
             <div>Paginação</div>
         </div>
     )
