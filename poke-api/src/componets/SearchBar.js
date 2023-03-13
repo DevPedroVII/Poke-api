bbad63970e5d8f4419f2b47664542edf021f2982
pedro Vinicius <singed161@gmail.com>
2023-03-13 20:45:33 -0300

    Update SearchBar.js

--- poke-api/src/componets/SearchBar.js
+++ poke-api/src/componets/SearchBar.js
@@ -17,23 +17,23 @@ const SearchBar = () => {
         const result = await searchPokemon(pokemon)
         setPokemon(result)
     }
+
     return (
         <div className = "searchbar-container">
             <div className = "searchbar" >
-        <input placeholder="Buscar Pokemon" onChange={onChangeHandler} />
+               <input placeholder="Buscar Pokemon" onChange={onChangeHandler} />
             </div >
-    <div className="searchbar-bnt">
-        <button onClick={onbuttonClickHandler}>Buscar </button>
-    </div>
-{
-    !pokemon ? (
-        <div>
+            <div className="searchbar-bnt">
+              <button onClick={onbuttonClickHandler}>Buscar </button>
+            </div>
+         {!pokemon ? (
+            <div>
             <div> Nome: {pokemon.name} </div>
             <div> Peso: {pokemon.weight} </div>
             <img src ={pokemon.sprites.front_default} alt ={pokemon.name}/>
-        </div>
-    ) : null}
-        </div >
+            </div>
+         ) : null}
+         </div >
     )
 }
 export default SearchBar;
