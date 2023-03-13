311bc155f5cc2bd4a6e812e8b1ebb614dc079a73
pedro Vinicius <singed161@gmail.com>
2023-03-13 20:22:00 -0300

    Update App.js

--- poke-api/src/App.js
+++ poke-api/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 import NavBar from './componets/NavBar';
 import  Pokedex  from './componets/pokedex';
 import SearchBar from './componets/SearchBar';
 
 function App() {
+  const fetchPokemons= async () =>{
+    setLoading(true)
+  }
+  useEffect(() => {
+    console.log("carregou")
+    return () => {
+
+    }
+  }, [])
+
   return (
     <div>
       <NavBar />
