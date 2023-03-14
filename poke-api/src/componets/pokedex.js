7ef20949a9d024c0324b6a37ab42f48b25140e6a
pedro Vinicius <singed161@gmail.com>
2023-03-14 00:49:42 -0300

    add

--- poke-api/src/componets/pokedex.js
+++ poke-api/src/componets/pokedex.js
@@ -1,20 +1,28 @@
 import React from "react";
 
-const Pokedex = (props) =>{
-    const {pokemons, loading} = props;
-    return(
+const Pokedex = (props) => {
+    const { pokemons, loading } = props;
+    return (
         <div>
             <div className="pokedex-header">
-            <h1>Pokedex</h1>
-            <div>Paginação</div>
-           </div>
-             {loading ? (
-           <div>Carregando, slow...</div>
-             ): (
-            <div className="pokedex-grid"> </div>
+                <h1>Pokedex</h1>
+                <div>Paginação</div>
+            </div>
+            {loading ? (
+                <div>Carregando, slow...</div>
+            ) : (
+                <div className="pokedex-grid">
+                    {pokemons.map({pokemon,index} => {
+                        return (<div>
+                            <div>#{pokemon.id} <div/>
+                            <div>#{pokemon.name} <div/>
+                            <img ></img>
+                        </div>)
+                    })}
+                </div>
             )}
         </div>
-         )
+    )
 }
 
 export default Pokedex;
